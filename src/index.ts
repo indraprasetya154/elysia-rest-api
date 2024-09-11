cae474f5e6e72fe60127f2768aa286551e918512
Indra Prasetya <indraprasetya154@gmail.com>
2024-09-11 11:58:07 +0700

    using try catch

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().
-                get("/", () => "Hello Elysia.js")
-                .get('/user/:id', ({ params: { id }}) => id)
-                .get('/category/:id', ({ params: { id }}) => id)
-                .listen(3000);
+try {
+  const app = new Elysia().
+                  get("/", () => "Hello Elysia.js")
+                  .get('/user/:id', ({ params: { id }}) => id)
+                  .get('/category/:id', ({ params: { id }}) => id)
+                  .listen(3000);
 
-console.log(app)
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  console.log(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  );
+} catch (error: any) {
+  console.log(error.message)
+}
