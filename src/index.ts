2e048762159f04ea2c85e4de6a3de26d840ac24e
Indra Prasetya <indraprasetya154@gmail.com>
2024-09-11 10:49:17 +0700

    new dockerfile

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,11 @@
 import { Elysia } from "elysia";
 
-const portString = process.env.APP_PORT;
-if (!portString) {
-  throw new Error("APP_PORT is not defined");
-}
-
-const port = parseInt(portString, 10);
-if (isNaN(port)) {
-  throw new Error("APP_PORT is not a valid number");
-}
-
 const app = new Elysia().
                 get("/", () => "Hello Elysia.js")
                 .get('/user/:id', ({ params: { id }}) => id)
                 .get('/category/:id', ({ params: { id }}) => id)
-                .listen(port);
+                .listen(3000);
 
 console.log(
-  `🦊 ${process.env.APP_NAME} is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
