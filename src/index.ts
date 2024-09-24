48ff20652758d35358996eaaeb06399f6ddea13b
Indra Prasetya <indraprasetya154@gmail.com>
2024-09-24 14:35:34 +0700

    change url name

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { Elysia } from "elysia";
 try {
   const app = new Elysia().
                   get("/", () => "Rest API with Elysia.js")
-                  .get('/user/:id', ({ params: { id }}) => id)
-                  .get('/category/:id', ({ params: { id }}) => id)
+                  .get('/users/:id', ({ params: { id }}) => id)
+                  .get('/categories/:id', ({ params: { id }}) => id)
                   .listen({
                     port: 3000,
                     hostname: '0.0.0.0', // This allows external access, including from CapRover
